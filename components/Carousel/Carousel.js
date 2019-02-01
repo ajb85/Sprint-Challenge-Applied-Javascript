b23b3ad5b03cf5e19629eef50e7ceb29ce88cc48
ajb85 <ajbrush85@gmail.com>
2019-02-01 12:19:39 -0500

    Carousel is working and flips infinitely

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,28 @@
 class Carousel {
-
+  constructor(element) {
+    this.element = element;
+    this.displayPic = 0;
+    this.pics = Array.from(this.element.getElementsByTagName("img"));
+    this.leftBtn = this.element.querySelector(".left-button");
+    this.rightBtn = this.element.querySelector(".right-button");
+    this.leftBtn.addEventListener("click", this.showPic.bind(this, -1));
+    this.rightBtn.addEventListener("click", this.showPic.bind(this, 1));
+    this.showPic(0);
+  }
+  showPic(i) {
+    this.displayPic += i;
+    if (this.displayPic < 0) {
+      this.displayPic = this.pics.length - 1;
+    } else if (this.displayPic >= this.pics.length) {
+      this.displayPic = 0;
+    }
+    this.pics.forEach(pic => (pic.style.display = "none"));
+    this.pics[this.displayPic].style.display = "block";
+  }
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector(".carousel");
+new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
